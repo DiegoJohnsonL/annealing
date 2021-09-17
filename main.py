fcaf66fe69db29f8c310588c061b2b6819495966
DiegoJohnsonL <u201714835@upc.edu.pe>
2021-09-16 20:40:48 -0500

    The ned

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 import folium
+import matplotlib.pyplot
 from matplotlib.animation import FuncAnimation
 
 from department import Department
@@ -47,24 +48,32 @@ def save_html_map(route):
     peru_map.save(r"peru_map.html")
 
 
+def draw_complete_map(route):
+    url_data = "https://raw.githubusercontent.com/juaneladio/peru-geojson/master/peru_departamental_simple.geojson"
+    region_geojson = gpd.read_file(url_data)
+    c_map = ListedColormap(['white' for _ in range(25)], name='test')
+    region_geojson.plot(figsize=(20, 20), edgecolor='black', cmap=c_map)
+    plt.xlabel('longitude')
+    plt.ylabel('latitude')
+
+    for i in range(len(route) - 1):
+        plt.plot(route[i].longitude, route[i].latitude, marker='o', color="red")
+        x_values = [route[i].longitude, route[i + 1].longitude]
+        y_values = [route[i].latitude, route[i + 1].latitude]
+        plt.plot(x_values, y_values)
+    plt.plot(route[len(route) - 1].longitude, route[len(route) - 1].latitude, marker='o', color="red")
+    x_values = [route[len(route) - 1].longitude, route[0].longitude]
+    y_values = [route[len(route) - 1].latitude, route[0].latitude]
+    plt.plot(x_values, y_values)
+    plt.show()
+
+
 if __name__ == '__main__':
     departments = get_departments_list()
-    FuncAnimation()
-    best_route = simulated_annealing(departments, 50)
+    best_route = simulated_annealing(departments, 20000)
+    draw_complete_map(best_route)
     save_html_map(best_route)
-    # url_data = "https://raw.githubusercontent.com/juaneladio/peru-geojson/master/peru_departamental_simple.geojson"
-    # region_geojson = gpd.read_file(url_data)
-    # c_map = ListedColormap(['white' for _ in range(25)], name='test')
-    # region_geojson.plot(figsize=(20, 20), edgecolor='black', cmap=c_map)
-    # plt.xlabel('longitude')
-    # plt.ylabel('latitude')
-    # for i in range(23):
-    #     plt.plot(best_route[i].longitude, best_route[i].latitude, marker='o', color="red")
-    #     x_values = [best_route[i].longitude, best_route[i+1].longitude]
-    #     y_values = [best_route[i].latitude, best_route[i + 1].latitude]
-    #     plt.plot(x_values, y_values)
-    # plt.show()
-
+  
 
 
 
--- simulated_annealing.py
+++ simulated_annealing.py
@@ -1,9 +1,8 @@
 from department import Department, get_haversine_distance
 import random
 import math
-import geopandas as gpd
 import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
+import matplotlib.patches as mpatches
 
 
 def get_energy(route):
@@ -14,30 +13,31 @@ def get_energy(route):
     return energy
 
 
-def draw(route, data):
+def draw(route, temp, distance):
     plt.clf()
-    c_map = ListedColormap(['white' for _ in range(25)], name='test')
-    data.plot(figsize=(20, 20), edgecolor='black', cmap=c_map)
-    plt.xlabel('longitude')
-    plt.ylabel('latitude')
     for i in range(len(route) - 1):
         plt.plot(route[i].longitude, route[i].latitude, marker='o', color="red")
         x_values = [route[i].longitude, route[i + 1].longitude]
         y_values = [route[i].latitude, route[i + 1].latitude]
         plt.plot(x_values, y_values)
-    plt.draw()
-    plt.pause(.000001)
+    plt.plot(route[len(route) - 1].longitude, route[len(route) - 1].latitude, marker='o', color="red")
+    x_values = [route[len(route) - 1].longitude, route[0].longitude]
+    y_values = [route[len(route) - 1].latitude, route[0].latitude]
+    plt.plot(x_values, y_values)
+    red_patch = mpatches.Patch(color="red", label=f'Temperatura: {round(temp, 6)}')
+    blue_patch = mpatches.Patch(color="blue",label=f'Distancia: {round(distance, 6)}')
+    blue_patch = mpatches.Patch(color="blue",label=f'Distancia: {round(distance, 6)}')
+    plt.legend(handles=[red_patch,blue_patch])
+    plt.pause(.00000000000000000001)
 
 
 def simulated_annealing(route, iterations):
-    plt.ion()
-    url_data = "https://raw.githubusercontent.com/juaneladio/peru-geojson/master/peru_departamental_simple.geojson"
-    region_geojson = gpd.read_file(url_data)
     temp = 1000
     cooling_index = 0.003
     random.shuffle(route)
     t_actual = route.copy()
     t_best = route.copy()
+    e_best = get_energy(route)
     prob = 0
     for i in range(iterations):
         t_new = t_actual.copy()
@@ -51,11 +51,14 @@ def simulated_annealing(route, iterations):
             prob = math.exp((e_actual - e_new) / temp)
         if prob > random.randint(0, 1):
             t_actual = t_new
-        e_best = get_energy(t_best)
         if e_actual < e_best:
             t_best = t_actual.copy()
-        temp = (1 - cooling_index) * temp
-        #draw(t_actual, region_geojson)
+            e_best = get_energy(t_best)
+            draw(t_actual, temp, e_best)
 
-    print(str(get_energy(t_best)) + "km")
+        temp = (1 - cooling_index) * temp
+        print(f'Iteracion: {i}')
+
+    plt.close()
+    print("La ruta es de: " + str(round(get_energy(t_best), 4)) + "km")
     return t_best
